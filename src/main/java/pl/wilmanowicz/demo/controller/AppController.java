cee2b073c4204655534ae183b87d33f5dfec434f
MichalWilmanowicz <80915542+MichalWilmanowicz@users.noreply.github.com>
2022-01-17 13:13:51 +0100

    Update AppController.java

--- src/main/java/pl/wilmanowicz/demo/controller/AppController.java
+++ src/main/java/pl/wilmanowicz/demo/controller/AppController.java
@@ -17,12 +17,21 @@ public class AppController {
     public AppController(AppService appService) {
         this.appService = appService;
     }
+//     @GetMapping
+//     @ResponseBody
+//     public Map<String, List<String>> getOffers(@PathVariable String city, @PathVariable String category, @PathVariable Integer pageNumber){
+// //        String city = "gdansk";
+// //        String category = "mieszkania";
+//         Map<String, List<String>> allData = appService.searchOffers(city, category, pageNumber);
+//         return allData;
 
+//     }
     @GetMapping
     @ResponseBody
-    public Map<String, List<String>> getOffers(@PathVariable String city, @PathVariable String category, @PathVariable Integer pageNumber){
-//        String city = "gdansk";
-//        String category = "mieszkania";
+    public Map<String, List<String>> getOffers(){
+        String city = "gdansk";
+        String category = "mieszkania";
+        Integer i = 1;
         Map<String, List<String>> allData = appService.searchOffers(city, category, pageNumber);
         return allData;
 
